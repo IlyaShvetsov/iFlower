47b5169d64a76d2980a91010fc99bf01050772b6
shvetsov ilya <shvetsov.ia@phystech.edu>
2020-11-24 13:45:32 +0300

    add MainActivity and navigation

---
+++ app/src/main/java/com/iflower/ui/search/SearchFragment.kt
@@ -0,0 +1,33 @@
+package com.iflower.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.iflower.R
+
+
+
+class SearchFragment : Fragment() {
+
+    private lateinit var homeViewModel: SearchViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(SearchViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_search, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, {
+            textView.text = it
+        })
+        return root
+    }
+
+}
