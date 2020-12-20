48fbb68074dbfe6f47b6d3e3d160f10e9a2adc61
shvetsov ilya <shvetsov.ia@phystech.edu>
2020-12-20 20:18:54 +0300

    add camera activity

--- app/src/main/java/com/iflower/photo/PhotoMainFragment.kt
+++ app/src/main/java/com/iflower/photo/PhotoMainFragment.kt
@@ -1,12 +1,14 @@
 package com.iflower.photo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.iflower.R
-import com.iflower.photo.ui.CameraFragment
+import com.iflower.photo.camera.CameraActivity
 
 
 
@@ -14,15 +16,15 @@ class PhotoMainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main_photo, container, false)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        childFragmentManager
-                .beginTransaction()
-                .replace(R.id.photo_container, CameraFragment())
-                .commit()
+        val root = inflater.inflate(R.layout.fragment_main_photo, container, false)
+
+        root.findViewById<Button>(R.id.btn_photo).setOnClickListener {
+            val intent = Intent(requireContext(), CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        return root
     }
 
 }
